f2ed400dfedd0b22cbc82a50e780879b2f485859
abifitrird <fratnadewi@gmail.com>
2020-12-01 13:48:06 +0700

    fixing, again

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
-const path = require("path");
 const express = require("express");
 const app = express();
 const multer = require("multer");
 const upload = multer({ dest: "./uploads/images/" });
 const fs = require("fs");
-// const fileUpload = require("express-fileupload");
 const cors = require("cors");
 // const port = 5000;
 const port = process.env.PORT || 5000;
-const publicPath = path.join(__dirname, "public");
 
 require("dotenv").config();
 
@@ -17,11 +14,6 @@ require("express-group-routes");
 const router = require("./routes/router");
 
 app.use(express.static("uploads/images"));
-app.use(express.static(publicPath));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(publicPath, "index.html"));
-});
 
 app.use(cors());
 
